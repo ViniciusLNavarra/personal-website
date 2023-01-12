3c4329cfade803f9a859c598d17970e76db48688
ViniciusLNavarra <52398261+ViniciusLNavarra@users.noreply.github.com>
2023-01-12 13:35:55 +0000

    Build: (ed53af0) Added forgotten rollupOptions in vite.config.js

---
+++ assets/contacts-b24d4780.js
@@ -0,0 +1,1 @@
+import{j as t,a as e,c as n,R as c}from"./client-b00605f0.js";/* empty css              */function r(){return t("div",{className:"App",children:[e("h1",{children:"Contacts"}),e("h2",{children:"GitHub"}),e("h2",{children:"E-Mail"}),e("h2",{children:"LinkedIn"})]})}n.createRoot(document.getElementById("root")).render(e(c.StrictMode,{children:e(r,{})}));
